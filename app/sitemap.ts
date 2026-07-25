2ac2431f114230a8474d11c71537f94a96cc1c9e
Gazi Sayem Uddin Samir <sayemuddinsamir00@gmail.com>
2026-07-25 21:39:15 +0600

    Update sitemap.ts

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -1,1 +1,13 @@
+import { MetadataRoute } from 'next';
 
+export default function sitemap(): MetadataRoute.Sitemap {
+  const baseUrl = 'https://samir.dev';
+  return [
+    {
+      url: baseUrl,
+      lastModified: new Date(),
+      changeFrequency: 'monthly',
+      priority: 1,
+    },
+  ];
+}
